edde5b312d2717e26a63ed6ddc101cc7e446ee51
ortfero <ortfero@gmail.com>
2016-07-06 21:00:34 +0400

    Permanent history

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Telegram = require('node-telegram-bot-api');
 var CircularBuffer = require('circular-buffer');
+var Files = require('fs');
 
 console.log('Starting @meightbot...');
 
@@ -9,6 +10,7 @@ var bot = new Telegram(token, botOptions);
 var baseurl = 'https://meightbot.herokuapp.com';
 var webHook = baseurl + '/' + token;
 var historyLength = 100;
+var historySaveInterval = 60000; // every minute
 
 console.log('Web hook at ', webHook);
 bot.setWebHook(webHook);
@@ -37,6 +39,24 @@ var predictionsRu = [
 ];
 
 var history = new CircularBuffer(historyLength);
+Files.readFile('history.txt', (err, data) => {
+  if(err)
+    return console.log('Unable to read history');
+  var lines = data.toString().split('\n');
+  var n = lines.length;
+  for(var i = 0; i < n; i++)
+    history.enq(lines[i]);
+  console.log('History was restored');
+});
+setInterval(function() {
+  var array = history.toarray();
+  var text = array.join('\n');
+  if(text.length === 0)
+    return;
+  Files.writeFile("history.txt", text, function(err) {
+    if(err) return console.log('Unable to save history');
+  }
+}, historySaveInterval);
 
 function prediction() {
   return predictionsRu[Math.floor(Math.random() * predictionsRu.length)];
@@ -79,10 +99,13 @@ bot.on('inline_query', function(inlineQuery) {
 function showHistory(chatId) {
   var array = history.toarray();
   var text = array.join('\n');
+  if(text.length === 0)
+    bot.sendMessage(chatId, "No history yet");
   bot.sendMessage(chatId, text);
 }
 
 function formatMessage(msg) {
   var date = new Date(msg.date * 1000);
+  var name = !!msg.from.username ? msg.from.username : msg.from.first_name + ' ' + msg.from.last_name;
   return '[' + date.toUTCString() + '] @' + msg.from.username + ' : ' + msg.text;
 }
